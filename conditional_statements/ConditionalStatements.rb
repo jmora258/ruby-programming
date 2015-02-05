5fe0e1477b710b483f2a3c5f1d0495028f264c00
jmora258 <jmora258@ucla.edu>
2015-02-05 15:15:26 -0800

    Add simple 'if' and 'else if' statements.

--- conditional_statements/ConditionalStatements.rb
+++ conditional_statements/ConditionalStatements.rb
@@ -1,7 +1,7 @@
-# Conditiona Statements 
+# CONDITIONAL STATEMENTS 
 
 
-
+# Sample 'if' statement 
 x = 5 
 y = 20 
 
@@ -9,3 +9,19 @@ if x == 5 then
 	y = 3
 	puts y
 end
+
+# inspect: 
+p y
+
+# -----------------------------------
+
+# Nested 'if else' statement
+if x == 10 then 
+	y = 50
+elsif x == 5 then
+	y = 100
+else 
+	y = 42 
+end 
+
+puts y 
